9f77382a9fbce86ee7589d18fcff777fc5a47061
Smailey Alexandre Behm <smailey.behm@gmail.com>
2025-04-01 09:22:11 -0300

    feat(frontend): alteração da página para inclusão do readme dos projetos via componentes próprios

--- frontend/src/app/(paginas)/projeto/[id]/page.tsx
+++ frontend/src/app/(paginas)/projeto/[id]/page.tsx
@@ -1,7 +1,9 @@
+import Readme from "@/components/projetos/Readme"
 import Cabecalho from "@/components/shared/Cabecalho"
 import CarrosselImagens from "@/components/shared/CarrosselImagens"
 import Container from "@/components/shared/Container"
 import Tecnologias from "@/components/tecnologias/tecnologias"
+import { obterReadme } from "@/functions/github"
 import { obterProjeto } from "@/functions/projetos"
 import { Projeto } from "@core"
 
@@ -9,14 +11,19 @@ export default async function PaginaProjeto(props: { params: Promise<{ id: strin
 	const { id } = await props.params
 	const projeto = (await obterProjeto(id)) as Projeto
 
-	return projeto ? (
+	if (!projeto) return null
+
+	const readme = await obterReadme(projeto.repositorio)
+
+	return (
 		<div className="bg-black">
 			<Cabecalho />
 			<Container className="py-7 flex flex-col items-center gap-10">
 				<h1 className="text-3xl font-bold self-start">{projeto.nome}</h1>
 				<CarrosselImagens imagens={projeto.imagens.slice(1)} />
 				<Tecnologias dados={projeto.tecnologias} tamanhoMenor />
+				<Readme conteudo={readme} />
 			</Container>
 		</div>
-	) : null
+	)
 }

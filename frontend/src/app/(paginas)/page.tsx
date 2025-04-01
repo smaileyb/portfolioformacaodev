63da7070b2060ed7de95bcd4e93e1091187abb68
Smailey Alexandre Behm <smailey.behm@gmail.com>
2025-04-01 08:24:45 -0300

    feat(frontend): inclusão dos componentes criados e estilização da página inicial

--- frontend/src/app/(paginas)/page.tsx
+++ frontend/src/app/(paginas)/page.tsx
@@ -1,5 +1,25 @@
 // Code: Home page
 
-export default function Home() {
-	return <div>Inicio</div>
+import Curriculo from "@/components/curriculo"
+import Principal from "@/components/landing/Principal"
+import Projetos from "@/components/projetos/Projetos"
+import Container from "@/components/shared/Container"
+import { obterProjetos } from "@/functions/projetos"
+import { obterTecnologias } from "@/functions/tecnologias"
+
+export default async function Home() {
+	const tecnologias = await obterTecnologias()
+	const projetos = await obterProjetos()
+	return (
+		<div>
+			<Principal tecnologias={tecnologias.destaques} />
+			<Container className="py-16 flex flex-col gap-10 items-center">
+				<Projetos titulo="Destaque" listaProjetos={projetos.destaques} />
+				<Projetos titulo="Web" listaProjetos={projetos.web} />
+				<Projetos titulo="Mobile" listaProjetos={projetos.mobile} />
+				<Projetos titulo="Jogos" listaProjetos={projetos.jogos} />
+				<Curriculo tecnologias={tecnologias.todas} />
+			</Container>
+		</div>
+	)
 }

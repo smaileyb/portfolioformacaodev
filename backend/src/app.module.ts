46345a8d15aa70ebaa26d75dc59bf3bd2ff78163
Smailey Alexandre Behm <smailey.behm@gmail.com>
2025-04-07 08:42:06 -0300

    chore(backend): atualizações nas configurações em razão da nova rota do agente de IA

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from "./app.controller"
 import { DbModule } from './db/db.module';
 import { ProjetoModule } from './projeto/projeto.module';
 import { TecnologiaModule } from './tecnologia/tecnologia.module';
+import { AssistenteModule } from './assistente/assistente.module';
 
 @Module({
-	imports: [DbModule, ProjetoModule, TecnologiaModule],
+	imports: [DbModule, ProjetoModule, TecnologiaModule, AssistenteModule],
 	controllers: [AppController],
 })
 export class AppModule {}
